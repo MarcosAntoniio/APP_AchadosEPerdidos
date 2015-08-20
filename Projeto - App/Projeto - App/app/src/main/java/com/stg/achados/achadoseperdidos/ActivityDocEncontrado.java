bba470ddf28cf1e5d90036b0fd267142c50c1aa9
marcos antonio <marcos.antonio.contato@outlook.com>
2015-08-19 22:18:18 -0300

    Estrutura criada

---
+++ Projeto - App/Projeto - App/app/src/main/java/com/stg/achados/achadoseperdidos/ActivityDocEncontrado.java
@@ -0,0 +1,20 @@
+package com.stg.achados.achadoseperdidos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Diego on 17/08/2015.
+ */
+public class ActivityDocEncontrado extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_doc_encontrado);
+
+    }
+}
