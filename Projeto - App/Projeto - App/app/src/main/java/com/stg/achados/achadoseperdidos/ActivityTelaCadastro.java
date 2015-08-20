c18c74966dd635f6f48cc815650fd9e0261a5a2b
marcos antonio <marcos.antonio.contato@outlook.com>
2015-08-20 14:34:20 -0300

    método

--- Projeto - App/Projeto - App/app/src/main/java/com/stg/achados/achadoseperdidos/ActivityTelaCadastro.java
+++ Projeto - App/Projeto - App/app/src/main/java/com/stg/achados/achadoseperdidos/ActivityTelaCadastro.java
@@ -5,24 +5,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.view.View;
+import android.widget.EditText;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  * Created by Diego on 17/08/2015.
  */
 public class ActivityTelaCadastro extends Activity {
 
+    EditText nome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
+        nome = (EditText)FindViewById(R.id.edtNome);
+
+
     }
 
-    public void Cadastrar(View view)
-    {
 
-         Intent intent = new Intent (this,ActivityDocEncontrado.class);
-        startActivity(intent);
-
+    public void Cadastrar(View view) {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+        String conexao = 
+        Connection conexao = DriverManager.getConnection()
 
     }
 
